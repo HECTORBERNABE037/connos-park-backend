aac3f71ab1da6126f9839524ed46a21e7abebfa5
HectorBernabe <iscjbernabe037@accitesz.com>
2025-11-14 17:21:27 -0600

    fix:quita securityScheme import

--- config/urls.py
+++ config/urls.py
@@ -28,7 +28,7 @@ from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 from django.http import JsonResponse
 from rest_framework_simplejwt.authentication import JWTAuthentication
-from drf_yasg.openapi import SecurityScheme
+#from drf_yasg.openapi import SecurityScheme
 
 schema_view = get_schema_view(
    openapi.Info(
@@ -41,13 +41,6 @@ schema_view = get_schema_view(
    public=True,
    permission_classes=(permissions.AllowAny,),
    authentication_classes=(JWTAuthentication, ),
-   security_definitions={
-        'Bearer': SecurityScheme(
-            type=openapi.TYPE_HTTP,
-            scheme='bearer',
-            bearer_format='JWT'
-        )
-    }
 )
 
 urlpatterns = [
