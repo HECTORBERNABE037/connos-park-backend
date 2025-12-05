f760546c211fd0c6221c151b69a37b4533d563b7
HectorBernabe <iscjbernabe037@accitesz.com>
2025-12-04 20:33:58 -0600

    Fix: Agregar archivos faltantes de la app avisos

--- avisos/apps.py
+++ avisos/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 class AvisosConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'apps.avisos'
-    label = 'avisos'
+    label = 'avisos'#
