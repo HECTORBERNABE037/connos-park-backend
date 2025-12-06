bb55a96cf30f938a88cea73acdefbaa555838d2f
HectorBernabe <iscjbernabe037@accitesz.com>
2025-12-05 20:22:55 -0600

    Agregar configuracion para la busqueda dinamica de usuarios

--- apps/clientes/views.py
+++ apps/clientes/views.py
@@ -1,4 +1,4 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, filters
 from rest_framework.permissions import IsAuthenticated
 from .models import Cliente, Vehiculo, Documento
 from .serializers import ClienteSerializer, VehiculoSerializer, DocumentoSerializer
@@ -7,6 +7,10 @@ class ClienteViewSet(viewsets.ModelViewSet):
     queryset = Cliente.objects.all()
     serializer_class = ClienteSerializer
     permission_classes = [IsAuthenticated]
+
+    #para el buscador
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['nombre', 'correo', 'telefono'] # Campos donde buscará
 
 class VehiculoViewSet(viewsets.ModelViewSet):
     queryset = Vehiculo.objects.all()
