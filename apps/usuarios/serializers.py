cbf0d18a88bd2cf0ac1fac7c2cbf64d223182586
HectorBernabe <iscjbernabe037@accitesz.com>
2025-12-05 20:05:51 -0600

    encriptar contrasena de usuarios

--- apps/usuarios/serializers.py
+++ apps/usuarios/serializers.py
@@ -8,19 +8,49 @@ class RolSerializer(serializers.ModelSerializer):
         fields = ['id', 'nombre', 'descripcion']
 
 class UsuarioSerializer(serializers.ModelSerializer):
+    #campo colo lectura para mostrar el objeto anidado
     rol = RolSerializer(read_only=True)
+    #campo de escritura para recibir el ID del rol
     rol_id = serializers.PrimaryKeyRelatedField(
         queryset=Rol.objects.all(),
         source='rol', write_only=True
     )
+    password = serializers.CharField(write_only=True, required=False)
 
     class Meta:
         model = Usuario
         fields = [
             'id', 'username', 'email', 'first_name', 'last_name',
-            'rol', 'rol_id', 'telefono', 'activo', 'is_superuser', 'is_staff'
+            'rol', 'rol_id', 'telefono', 'activo','password','is_superuser', 'is_staff'
         ]
         read_only_fields = ['id', 'is_superuser', 'is_staff']
+
+# SOBRESCRIBIR CREATE: Para encriptar contraseña al crear
+    def create(self, validated_data):
+        password = validated_data.pop('password', None)
+        # aunque el source='rol' ya lo maneja, aseguramos
+        instance = self.Meta.model(**validated_data)
+        
+        if password:
+            instance.set_password(password) # aqui se encripta
+        
+        instance.save()
+        return instance
+
+    # SOBRESCRIBIR UPDATE: Para encriptar contraseña al editar (si se envía)
+    def update(self, instance, validated_data):
+        password = validated_data.pop('password', None)
+        
+        # Actualizar el resto de campos
+        for attr, value in validated_data.items():
+            setattr(instance, attr, value)
+            
+        if password:
+            instance.set_password(password) # Encripta la nueva contraseña
+            
+        instance.save()
+        return instance
+
 
 class CustomTokenObtainPairSerializer(TokenObtainPairSerializer):
     """
